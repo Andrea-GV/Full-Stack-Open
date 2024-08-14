8b704a45e6f1526675bc2e4f06db29de7672e49d
Andrea-GV <andrea.garcia.valverde@bootcamp-upgrade.com>
2024-08-14 19:09:06 +0200

    added and finished 1.6 unicafe opinion counters

---
+++ part1/1.6-1.14/Unicafe/src/App.jsx
@@ -0,0 +1,37 @@
+// import React from 'react'
+import { useState } from "react";
+
+const App = () => {
+
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGoodClick = () => {
+    setGood(good + 1);
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1);
+  }
+
+  return (
+    <div>
+      <h1>Give your feedback</h1>
+      <button onClick={handleGoodClick}>Good</button>
+      <button onClick={handleNeutralClick}>Neutral</button>
+      <button onClick={handleBadClick}>Bad</button>
+
+      <h1>Overall</h1>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+    </div>
+  )
+}
+
+export default App
